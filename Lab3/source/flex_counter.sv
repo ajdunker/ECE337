77b17e1ca0c799ecf366417fb0dffc7687928352
Alex Dunker <adunker@purdue.edu>
2016-02-04 13:55:35 -0500

    Initial upload

---
+++ Lab3/source/flex_counter.sv
@@ -0,0 +1,7 @@
+// $Id: $
+// File name:   flex_counter.sv
+// Created:     2/4/2016
+// Author:      Alexander Dunker
+// Lab Section: 337-04
+// Version:     1.0  Initial Design Entry
+// Description: Flexible and Scalable Counter with Controlled Rollover.
---
+++ Lab4/source/flex_stp_sr.sv
@@ -0,0 +1,7 @@
+// $Id: $
+// File name:   flex_stp_sr.sv
+// Created:     2/3/2016
+// Author:      Alexander Dunker
+// Lab Section: 337-04
+// Version:     1.0  Initial Design Entry
+// Description: N-bit Serial-to-Parallel Shift Register Design.
