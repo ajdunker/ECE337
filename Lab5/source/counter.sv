24fa779a2dd7158a7f73e8db7a2414b7447cbb51
Alex Dunker <adunker@purdue.edu>
2016-02-17 18:16:46 -0500

    Pre Lab 5

---
+++ Lab5/source/counter.sv
@@ -0,0 +1,7 @@
+// $Id: $
+// File name:   counter.sv
+// Created:     2/17/2016
+// Author:      Alexander Dunker
+// Lab Section: 337-04
+// Version:     1.0  Initial Design Entry
+// Description: Counter unit.
---
+++ Lab5/source/fir_filter.sv
@@ -0,0 +1,7 @@
+// $Id: $
+// File name:   fir_filter.sv
+// Created:     2/17/2016
+// Author:      Alexander Dunker
+// Lab Section: 337-04
+// Version:     1.0  Initial Design Entry
+// Description: Top level.
