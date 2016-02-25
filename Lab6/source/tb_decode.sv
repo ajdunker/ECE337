5821d1fd42cf659107c125ab29a830555f5a0b64
Alex Dunker <adunker@purdue.edu>
2016-02-25 00:35:24 -0500

    test bench commits

--- Lab6/source/tb_decode.sv
+++ Lab6/source/tb_decode.sv
@@ -6,6 +6,8 @@
 // Version:     1.0  Initial Design Entry
 // Description: Test bench for decode
 
+`timescale 1ns / 10ps
+
 module tb_decode
   ();
 
@@ -41,8 +43,91 @@ module tb_decode
 	@(posedge tb_clk);
 	tb_n_rst = 1;
 	@(posedge tb_clk);
-	@(negedge tb_clk);
 	@(posedge tb_clk);
+
+	tb_dplus = 1;
+	@(posedge tb_clk);
+	@(negedge tb_clk);
+	assert(tb_dorig == 0) begin
+	   $info("PASS 1");
+	end else begin
+	   $info("FAIL 1");
+	end
+
+	@(posedge tb_clk);
+	tb_se = 1;
+	@(posedge tb_clk);
+	tb_se = 0;
+	@(posedge tb_clk);
+	@(negedge tb_clk);
+	assert(tb_dorig == 1) begin
+	   $info("PASS 2");
+	end else begin
+	   $info("FAIL 2");
+	end
+
+	@(posedge tb_clk);
+	tb_dplus = 0;
+	@(posedge tb_clk);
+	@(negedge tb_clk);
+	assert(tb_dorig == 0) begin
+	   $info("PASS 3");
+	end else begin
+	   $info("FAIL 3");
+	end
+
+	@(posedge tb_clk);
+	tb_se = 1;
+	tb_eop = 1;
+	@(posedge tb_clk);
+	tb_se = 0;
+	tb_eop = 0;
+	@(posedge tb_clk);
+	@(negedge tb_clk);
+	assert(tb_dorig == 0) begin
+	   $info("PASS 4");
+	end else begin
+	   $info("FAIL 4");
+	end
+	@(posedge tb_clk);
+	tb_dplus = 1;
+	@(posedge tb_clk);
+	@(negedge tb_clk);
+	assert(tb_dorig == 1) begin
+	   $info("PASS 5");
+	end else begin
+	   $info("FAIL 5");
+	end
+	@(posedge tb_clk);
+	tb_dplus = 0;
+	@(posedge tb_clk);
+	@(negedge tb_clk);
+	assert(tb_dorig == 0) begin
+	   $info("PASS 6");
+	end else begin
+	   $info("FAIL 6");
+	end
+	@(posedge tb_clk);
+	tb_dplus = 1;
+	@(posedge tb_clk);
+	@(negedge tb_clk);
+	assert(tb_dorig == 1) begin
+	   $info("PASS 7");
+	end else begin
+	   $info("FAIL 7");
+	end
+	
+	@(posedge tb_clk);
+	tb_se = 1;
+	@(posedge tb_clk);
+	tb_se = 0;
+	@(posedge tb_clk);
+	@(negedge tb_clk);
+	assert(tb_dorig == 1) begin
+	   $info("PASS 8");
+	end else begin
+	   $info("FAIL 8");
+	end
 	
      end
    
